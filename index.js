404e920c206d6c63711ff358bcee80ad55c6eef9
admin <admin@admins-MacBook-Pro.local>
2022-02-25 16:29:54 -0500

    Checking new configuration

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to funny exchange" });
+});
 
 app.listen(PORT, () => {
   console.log(`App listen on port ${PORT}`);
